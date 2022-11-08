ca71f438ff56516e40b9fd3d53c0b2304b60b19e
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-11-08 12:26:14 +0600

    relational and logical operators, conditional statements

---
+++ Operators/src/org/paul/operators/Main.java
@@ -0,0 +1,34 @@
+package org.paul.operators;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 9;
+
+
+//        boolean answer = a != b;
+//        System.out.println(answer);
+//
+////        a = a+2;
+//
+////        a += 2; (same as a++ increase 2)
+//
+////        a++;
+//
+//        System.out.println(a);
+//
+//        boolean ans = a==5 || b==3;
+//        System.out.println(ans);
+
+
+        boolean ans = a >3;
+        if (ans){
+            System.out.println("A is greater than 3");
+        } else {
+            System.out.println("A is less than 3");
+        }
+
+        //System.out.println("Continue.....");
+
+    }
+}
