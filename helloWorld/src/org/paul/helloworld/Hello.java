018e4ed564606136118bebcb0b4195cfd4f42d81
Sourav Paul <35699196+SouravPall@users.noreply.github.com>
2022-11-08 11:17:17 +0600

    variables and arithmetic operators

--- helloWorld/src/org/paul/helloworld/Hello.java
+++ helloWorld/src/org/paul/helloworld/Hello.java
@@ -2,6 +2,39 @@ package org.paul.helloworld;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+//        System.out.println("Hello World");
+//        System.out.print("Hello!");
+
+        int number = -5;
+
+        System.out.println(number);
+
+        long myLong = 5;
+        System.out.println(myLong);
+
+        double myDouble = 4.5;
+        System.out.println(myDouble);
+
+        float myFloat = 4.5F;
+        System.out.println(myFloat);
+
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        String myString = "Hello World";
+        System.out.println(myString);
+
+
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+
+
+        int a = 6;
+        int b = 5;
+
+        int answer = a + b;
+        System.out.println("Sum: "+ answer);
+
+
     }
 }
